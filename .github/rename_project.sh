41a703447410f950004b5fcaab20a6f9c1fc3d8c
lt8cn <lt8cn@users.noreply.github.com>
2022-02-26 02:25:51 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="lt8cn"
+original_name="sanfoo"
+original_urlname="sanfoo"
+original_description="Awesome sanfoo created by lt8cn"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv sanfoo $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- sanfoo/app.py
+++ sanfoo/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+sanfoo API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="sanfoo",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://sanfoo.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "lt8cn",
+        "url": "http://sanfoo.com/contact/",
+        "email": "lt8cn@sanfoo.com",
     },
     license_info={
         "name": "The Unlicense",
